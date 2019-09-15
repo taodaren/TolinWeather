b3b539892900bd44c3b270db53c0b1f5f416dcff
taodaren <taodaren1125@gmail.com>
2019-09-15 11:47:25 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/tolinweather/android/db/County.java
@@ -0,0 +1,43 @@
+package com.tolinweather.android.db;
+
+import org.litepal.crud.LitePalSupport;
+
+public class County extends LitePalSupport {
+
+    private int id;
+    private String countyName;// 记录县的名字
+    private int weatherId;    // 记录县所对应的天气 ID
+    private int cityId;       // 记录当前市所属市的 ID 值
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public int getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(int weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
